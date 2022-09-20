0d9cf768f34576cd09fdef222cfa1230c14222ed
Dinesh Kumar <dineshkumar757506@gmail.com>
2022-09-20 13:14:29 +0530

    Create a program for prime number

---
+++ prime.js
@@ -0,0 +1,15 @@
+function checkPrime(num){
+    let count = 0
+    for(let i = 1; i <= num; i++){
+        if(num%j==0){
+            count++
+        }
+    }
+    if(count > 2){
+        console.log(`${num} is a prime number`)
+    }else{
+        console.log(`${num} is a not prime number`)
+    }
+}
+
+checkPrime(4)
