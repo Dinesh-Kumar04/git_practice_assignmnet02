2dbdb5e2541277deae438b5c201fc1f9a45eece0
Dinesh Kumar <dineshkumar757506@gmail.com>
2022-09-20 13:50:50 +0530

    Fixed the code by Dinesh-Kumar04

--- prime.js
+++ prime.js
@@ -1,14 +1,14 @@
 function checkPrime(num){
-    let num1 = 0
+    let count = 0
     for(let i = 1; i <= num; i++){
-        if(num%j==0){
-            num1++
+        if(num%i==0){
+            count++
         }
     }
-    if(num1 > 2){
-        console.log(`${num} is a prime number`)
-    }else{
+    if(count > 2){
         console.log(`${num} is a not prime number`)
+    }else{
+        console.log(`${num} is a prime number`)
     }
 }
 
